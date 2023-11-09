05cab8d17ec79f3eac2ef73f32303eb428e580ab
pwh-pwh <plmoknpwh@163.com>
2023-11-09 16:14:17 +0800

    hook startActivity

--- app/src/main/java/dev/coderpwh/fuck/hook/HookEntry.kt
+++ app/src/main/java/dev/coderpwh/fuck/hook/HookEntry.kt
@@ -1,9 +1,12 @@
 package dev.coderpwh.fuck.hook
 
+import android.app.Activity
+import android.content.Intent
 import android.hardware.SensorManager
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.type.android.ActivityClass
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed(modulePackageName = "dev.coderpwh.fuck.hook")
@@ -21,6 +24,20 @@ object HookEntry:IYukiHookXposedInit {
                     paramCount = 3
                 }
                 replaceToTrue()
+            }
+        }
+        ActivityClass.hook {
+            injectMember {
+                method {
+                    name = "startActivity"
+                }
+                replaceUnit {
+                    val intent = args(0).cast<Intent>()!!
+                    val pName = intent.component?.packageName
+                    if(pName == packageName) {
+                        callOriginal()
+                    }
+                }
             }
         }
     }
