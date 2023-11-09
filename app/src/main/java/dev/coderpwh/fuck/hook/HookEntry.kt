5b3f3a4279c7a8f2b814c8624897279427f6e7d4
pwh-pwh <plmoknpwh@163.com>
2023-11-09 16:52:43 +0800

    hook startActivityForResult

--- app/src/main/java/dev/coderpwh/fuck/hook/HookEntry.kt
+++ app/src/main/java/dev/coderpwh/fuck/hook/HookEntry.kt
@@ -6,6 +6,7 @@ import android.hardware.SensorManager
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.log.loggerI
 import com.highcapable.yukihookapi.hook.type.android.ActivityClass
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
@@ -14,6 +15,9 @@ object HookEntry:IYukiHookXposedInit {
 
     override fun onInit() = configs {
         isDebug = false
+        debugLog {
+            tag = "[fuck_shake]"
+        }
     }
 
     override fun onHook() = encase {
@@ -32,8 +36,25 @@ object HookEntry:IYukiHookXposedInit {
                     name = "startActivity"
                 }
                 replaceUnit {
+                    loggerI(msg = "startActivity invoke")
                     val intent = args(0).cast<Intent>()!!
                     val pName = intent.component?.packageName
+                    loggerI(msg = "from packageName:$packageName to pName:${pName}")
+                    if(pName == packageName) {
+                        callOriginal()
+                    }
+                }
+            }
+
+            injectMember {
+                method {
+                    name = "startActivityForResult"
+                }
+                replaceUnit {
+                    loggerI(msg = "startActivityForResult invoke")
+                    val intent = args(0).cast<Intent>()!!
+                    val pName = intent.component?.packageName
+                    loggerI(msg = "from packageName:$packageName to pName:${pName}")
                     if(pName == packageName) {
                         callOriginal()
                     }
