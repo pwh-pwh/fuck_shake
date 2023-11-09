5ce398f958a177087b0e1eb1390da03669b3ce9a
pwh-pwh <plmoknpwh@163.com>
2023-11-09 15:02:57 +0800

    Remove unnecessary dependencies and remove the activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
         applicationId = "dev.coderpwh.fuck"
         minSdk = 29
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -44,13 +44,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.8.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.annotation:annotation-jvm:1.7.0")
 
     // 基础依赖
     implementation("com.highcapable.yukihookapi:api:1.1.11")
